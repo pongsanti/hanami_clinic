53319eb96b78a1806aab79f1150324ba0d707aed
pongsanti <pongsanti.tanvejsilp@gmail.com>
2016-12-10 20:08:25 +0700

    first commit

---
+++ config/environment.rb
@@ -0,0 +1,40 @@
+require 'bundler/setup'
+require 'hanami/setup'
+require 'hanami/model'
+require_relative '../lib/hanami_clinic'
+require_relative '../apps/web/application'
+
+Hanami.configure do
+  mount Web::Application, at: '/'
+
+  model do
+    ##
+    # Database adapter
+    #
+    # Available options:
+    #
+    #  * SQL adapter
+    #    adapter :sql, 'sqlite://db/hanami_clinic_development.sqlite3'
+    #    adapter :sql, 'postgres://localhost/hanami_clinic_development'
+    #    adapter :sql, 'mysql://localhost/hanami_clinic_development'
+    #
+    adapter :sql, ENV['DATABASE_URL']
+
+    ##
+    # Migrations
+    #
+    migrations 'db/migrations'
+    schema     'db/schema.sql'
+  end
+
+  mailer do
+    root 'lib/hanami_clinic/mailers'
+
+    # See http://hanamirb.org/guides/mailers/delivery
+    delivery do
+      development :test
+      test        :test
+      # production :smtp, address: ENV['SMTP_PORT'], port: 1025
+    end
+  end
+end
