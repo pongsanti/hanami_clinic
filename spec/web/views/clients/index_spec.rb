d90f87143952b739b6e02631c3fa41649ea83898
pongsanti <pongsanti.tanvejsilp@gmail.com>
2016-12-19 11:01:25 +0700

    clients index template and spec

--- spec/web/views/clients/index_spec.rb
+++ spec/web/views/clients/index_spec.rb
@@ -2,12 +2,34 @@ require 'spec_helper'
 require_relative '../../../../apps/web/views/clients/index'
 
 describe Web::Views::Clients::Index do
-  let(:exposures) { Hash[foo: 'bar'] }
-  let(:template)  { Hanami::View::Template.new('apps/web/templates/clients/index.html.erb') }
+  let(:exposures) { Hash[clients: []] }
+  let(:template)  { Hanami::View::Template.new('apps/web/templates/clients/index.html.slim') }
   let(:view)      { Web::Views::Clients::Index.new(template, exposures) }
   let(:rendered)  { view.render }
 
-  it 'exposes #foo' do
-    view.foo.must_equal exposures.fetch(:foo)
+  it 'exposes #clients' do
+    view.clients.must_equal exposures.fetch(:clients)
+  end
+
+  describe 'when there are no clients' do
+    it 'shows a placeholder message' do
+      rendered.must_include('<p class="placeholder">There are no clients</p>')
+    end
+  end
+
+  describe 'where there are clients' do
+    let(:client01) { Client.new(name: 'Client A') }
+    let(:client02) { Client.new(name: 'Client B') }
+    let(:exposures) { Hash[clients: [client01, client02]] }
+
+    it 'lists them all' do
+      rendered.scan(/class="client"/).count.must_equal 2
+      rendered.must_include('Client A')
+      rendered.must_include('Client B')
+    end
+
+    it 'hides the placeholder message' do
+      rendered.wont_include('<p class="placeholder">There are no clients</p>')
+    end
   end
 end
